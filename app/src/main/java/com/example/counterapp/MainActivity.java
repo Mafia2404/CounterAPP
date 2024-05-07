d0d21398be12003e672474d3e873a534f1a5e511
Mafia_2404 <uddhavpawar48@gmail.com>
2024-05-07 17:59:48 +0530

    Conter App

---
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.counterapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+TextView welcomeTxt,CountTxt;
+Button btn;
+int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //writemycode
+        welcomeTxt=findViewById(R.id.textView1);
+        CountTxt=findViewById(R.id.textView2);
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+            CountTxt.setText(String.valueOf(counter()));
+                });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+        });
+    }
+    public int counter(){
+        return ++count;
+    }
+}
